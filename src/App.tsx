8f5f73a0eb22cfbf64aeb6f1e6097bf3dc9786ab
0xsarwagya <sarwagyasingh69@gmail.com>
2023-11-04 22:55:50 +0100

    Refactor : Fixed Bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import GitProfile from "@arifszn/gitprofile";
 import "@arifszn/gitprofile/dist/style.css";
 //@ts-ignore
-import config from "./gitprofileconfig.js";
+import { config } from "./gitprofileconfig.js";
 
 function App() {
   return <GitProfile config={config} />;
