a81d4ce6fc5026c49de12b6e82de6366700ea7ab
0xsarwagya <sarwagyasingh69@gmail.com>
2023-11-04 22:49:11 +0100

    Base Version : Added Website Config and Files

---
+++ gitprofile.config.js
@@ -0,0 +1,70 @@
+
+const config = {
+  github: {
+    username: "shivamkumarrrr",
+    sortBy: "updated",
+    limit: 8,
+    exclude: {
+      forks: false,
+      projects: [],
+    },
+  },
+  social: {
+    linkedin: "https://www.linkedin.com/in/shiiiivam/",
+    instagram: "https://www.instagram.com/shiivam_kumarr/",
+    email: "shivamkumar17june@gmail.com",
+  },
+  resume: {
+    fileUrl: "",
+  },
+  skills: ["JavaScript", "React.js"],
+  education: [
+    {
+      institution: "Universität des Saarlandes",
+      degree: "Bachelors degree",
+      from: "2023",
+      to: "2027",
+    },
+  ],
+  themeConfig: {
+    defaultTheme: "forest",
+    disableSwitch: false,
+    respectPrefersColorScheme: true,
+    hideAvatarRing: false,
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "procyon",
+    ],
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Copyright © 2023 John Doe`,
+};
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import GitProfile from "@arifszn/gitprofile";
+import "@arifszn/gitprofile/dist/style.css";
+
+function App() {
+  return (
+    <GitProfile
+      config={{
+        github: {
+          username: "shivamkumarrrr",
+        },
+      }}
+    />
+  );
+}
+
+export default App;
